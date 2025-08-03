df1c440fe3aefd6b1e7677c2aba77d2cab966bae
Raihan-iOS <md.fasi.raihan.ssl@gmail.com>
2025-08-03 10:51:15 +0600

    ..

---
+++ lib/module_9/module9_class2_drawer.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Module9class2Drawer extends StatefulWidget {
+  const Module9class2Drawer({super.key});
+
+  @override
+  State<Module9class2Drawer> createState() => _Module9class2DrawerState();
+}
+
+class _Module9class2DrawerState extends State<Module9class2Drawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/myApp.dart
+++ lib/myApp.dart
@@ -4,6 +4,7 @@ import 'package:widget_app_structure/module8/module8class1.dart';
 import 'package:widget_app_structure/module8/module8class2.dart';
 import 'package:widget_app_structure/module8/stackpractice.dart';
 import 'package:widget_app_structure/module_9/alert.dart';
+import 'package:widget_app_structure/module_9/module9_class2_drawer.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -14,9 +15,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: Alert(),
+      home: Module9class2Drawer(),
     );
   }
 }
